2e5eca758cfafd5f0be04037a5f3ffb9391c3e61
Wings <wings.xiangyi.zeng@gmail.com>
2024-05-15 14:51:55 +0800

    nvim: disable mason-nvim-dap

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -5,4 +5,5 @@ return {
   { "mfussenegger/nvim-dap", enabled = false },
   { "rcarriga/cmp-dap", enabled = false },
   { "rcarriga/nvim-dap-ui", enabled = false },
+  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
 }
