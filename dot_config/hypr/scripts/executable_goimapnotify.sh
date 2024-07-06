c6808bc9525a252167ae3c7d02347495d3bc6003
Wings <wings.xiangyi.zeng@gmail.com>
2024-07-06 15:57:07 +0800

    hyprland: set logger and pam-gnupg env for goimapnotify

--- dot_config/hypr/scripts/executable_goimapnotify.sh
+++ dot_config/hypr/scripts/executable_goimapnotify.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
+export GNUPGHOME="$XDG_DATA_HOME/gnupg"
+export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store"
 while true; do
-  goimapnotify
+  goimapnotify 2>&1 | logger -t goimapnotify
+  logger -t goimapnotify "crashed, restarting in 60 seconds"
   sleep 60
 done
