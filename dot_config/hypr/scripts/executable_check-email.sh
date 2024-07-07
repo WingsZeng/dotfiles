46d361003f2b60514097e1f8f5b086d33583bef6
Wings <wings.xiangyi.zeng@gmail.com>
2024-07-07 17:51:20 +0800

    hyprland: fix check email

--- dot_config/hypr/scripts/executable_check-email.sh
+++ dot_config/hypr/scripts/executable_check-email.sh
@@ -1,12 +1,15 @@
 #!/bin/zsh
 
+export GNUPGHOME="$XDG_DATA_HOME/gnupg"
+export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store"
+
 emails=(${(f)"$(ls $XDG_DATA_HOME/mail)"})
-echo $emails
 
 for email in $emails; do
-  echo $email
   mbsync -c $XDG_CONFIG_HOME/isync/isyncrc $email
-  if ! notmuch new | grep -q "No new mail"; then
+  result=$(notmuch new)
+  logger -t "check email" "$email: $result"
+  if ! echo $result | grep -q "No new mail"; then
     $XDG_CONFIG_HOME/goimapnotify/notify.sh $email
   fi
 done
