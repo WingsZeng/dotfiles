4157ab79510ec282edf1f3ae7278f4ca9f7ea231
Wings <wings.xiangyi.zeng@gmail.com>
2024-11-04 16:38:29 +0800

    nvim: migrate auto-save to latest version

--- dot_config/nvim/lua/plugins/auto-save.lua
+++ dot_config/nvim/lua/plugins/auto-save.lua
@@ -1,10 +1,6 @@
 return {
   "okuuva/auto-save.nvim",
-  event = { "InsertLeave", "TextChanged" },
   opts = {
-    execution_message = {
-      enabled = false,
-    },
     debounce_delay = 100,
   },
 }
