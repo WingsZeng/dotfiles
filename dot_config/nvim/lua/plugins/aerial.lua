796d16c387d86e3df1bb957d7b59360b4aad6986
Wings <wings.xiangyi.zeng@gmail.com>
2024-12-20 23:42:04 +0800

    nvim: show aerial but not focus

--- dot_config/nvim/lua/plugins/aerial.lua
+++ dot_config/nvim/lua/plugins/aerial.lua
@@ -6,7 +6,7 @@ return {
       mappings = {
         n = {
           ["<Leader>lS"] = false,
-          ["<C-O>"] = { function() require("aerial").toggle() end, desc = "Symbols outline" },
+          ["<C-O>"] = { function() require("aerial").toggle({ focus = false }) end, desc = "Symbols outline" },
         },
       },
     },
