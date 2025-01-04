0a3e4f74f1f2696bdc7e4d1829b910673cae2f92
Wings <wings.xiangyi.zeng@gmail.com>
2025-01-04 14:03:23 +0800

    nvim: fix cmp keymap

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -13,6 +13,8 @@ return {
   },
   opts = function(_, opts)
     local luasnip, cmp = require "luasnip", require "cmp"
+    if not opts.mappings then opts.mappings = {} end
+
     opts.mapping["<C-P>"] = nil
     opts.mapping["<C-N>"] = nil
     opts.mapping["<C-U>"] = nil
@@ -20,26 +22,6 @@ return {
     opts.mapping["<C-E>"] = nil
     opts.mapping["<C-K>"] = nil
     opts.mapping["<C-J>"] = nil
-
-    opts.mapping["<Down>"] = cmp.mapping(function(fallback)
-      if is_visible(cmp) then
-        cmp.select_next_item()
-      elseif vim.api.nvim_get_mode().mode ~= "c" and luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" })
-
-    opts.mapping["<Up>"] = cmp.mapping(function(fallback)
-      if is_visible(cmp) then
-        cmp.select_prev_item()
-      elseif vim.api.nvim_get_mode().mode ~= "c" and luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" })
 
     opts.mapping["<M-Space>"] = cmp.mapping(function()
       if cmp.visible() then
@@ -50,7 +32,6 @@ return {
     end
     )
 
-    if not opts.mappings then opts.mappings = {} end
     opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
       if is_visible(cmp) and has_words_before() then
         cmp.select_next_item()
