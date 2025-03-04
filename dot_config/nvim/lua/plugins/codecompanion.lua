6a7f0d6b3ded2aec388c90964ca3b30a1948fb99
Wings <wings.xiangyi.zeng@gmail.com>
2025-03-04 15:50:19 +0800

    nvim: use codecompanion, remove copilot

---
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,270 @@
+---@diagnostic disable: undefined-field
+-- if true then return {} end
+local M = {}
+
+local ns_id = vim.api.nvim_create_namespace "spinner"
+
+-- TODO: background color of spinner
+M.config = {
+  spinner = {
+    text = "",
+    hl_positions = {
+      { 0, 3 }, -- First circle
+      { 3, 6 }, -- Second circle
+      { 6, 9 }, -- Third circle
+    },
+    interval = 100,
+    hl_group = "CursorLineNr",
+    hl_dim_group = "LineNr",
+  },
+}
+
+local spinner_state = {
+  timer = nil,
+  win = nil,
+  buf = nil,
+  frame = 1,
+}
+
+local function create_spinner_window()
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf })
+
+  local win = vim.api.nvim_open_win(buf, false, {
+    relative = "cursor",
+    row = -1,
+    col = 0,
+    width = 3,
+    height = 1,
+    style = "minimal",
+    focusable = false,
+  })
+
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { M.config.spinner.text })
+
+  -- Set the dim highlight for the entire text
+  vim.api.nvim_buf_set_extmark(buf, ns_id, 0, 0, {
+    end_col = 9,
+    hl_group = M.config.spinner.hl_dim_group,
+    priority = vim.highlight.priorities.user - 1,
+  })
+
+  return buf, win, ns_id
+end
+
+function M.start_spinner()
+  if spinner_state.timer then return end
+
+  local buf, win = create_spinner_window()
+  spinner_state.buf = buf
+  spinner_state.win = win
+
+  spinner_state.timer = vim.loop.new_timer()
+  spinner_state.timer:start(
+    0,
+    M.config.spinner.interval,
+    vim.schedule_wrap(function()
+      if
+        spinner_state.win == nil
+        or spinner_state.buf == nil
+        or not (vim.api.nvim_win_is_valid(spinner_state.win) and vim.api.nvim_buf_is_valid(spinner_state.buf))
+      then
+        M.stop_spinner()
+        return
+      end
+
+      -- Update window position relative to cursor
+      local ok = pcall(vim.api.nvim_win_set_config, spinner_state.win, {
+        relative = "cursor",
+        row = -1,
+        col = 0,
+      })
+
+      -- If window update failed, stop the spinner
+      if not ok then
+        M.stop_spinner()
+        return
+      end
+      vim.api.nvim_buf_clear_namespace(spinner_state.buf, ns_id, 0, -1)
+
+      vim.api.nvim_buf_set_extmark(spinner_state.buf, ns_id, 0, 0, {
+        end_col = 9,
+        hl_group = M.config.spinner.hl_dim_group,
+        priority = vim.highlight.priorities.user - 1,
+      })
+
+      -- Set animation highlight
+      local hl_pos = M.config.spinner.hl_positions[spinner_state.frame]
+      vim.api.nvim_buf_set_extmark(spinner_state.buf, ns_id, 0, hl_pos[1], {
+        end_col = hl_pos[2],
+        hl_group = M.config.spinner.hl_group,
+        priority = vim.highlight.priorities.user + 1,
+      })
+
+      spinner_state.frame = (spinner_state.frame % #M.config.spinner.hl_positions) + 1
+    end)
+  )
+end
+
+function M.stop_spinner()
+  if spinner_state.timer then
+    spinner_state.timer:stop()
+    spinner_state.timer:close()
+    spinner_state.timer = nil
+  end
+
+  -- safely close the window if it exists and is valid
+  if spinner_state.win then
+    pcall(vim.api.nvim_win_close, spinner_state.win, true)
+    spinner_state.win = nil
+  end
+
+  if spinner_state.buf then
+    pcall(vim.api.nvim_buf_delete, spinner_state.buf, { force = true })
+    spinner_state.buf = nil
+  end
+  spinner_state.frame = 1
+end
+
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-treesitter/nvim-treesitter" },
+    { "echasnovski/mini.diff" },
+    { "ibhagwan/fzf-lua" },
+    {
+      "AstroNvim/astrocore",
+      ---@param opts AstroCoreOpts
+      opts = function(_, opts)
+        local maps = assert(opts.mappings)
+        local prefix = opts.options.g.copilot_chat_prefix or "<Leader>c"
+        local astroui = require "astroui"
+        local codecompanion = require "codecompanion"
+
+        maps.n[prefix] = { desc = astroui.get_icon("CodeCompanion", 1, true) .. "CodeCompanion" }
+        maps.v[prefix] = { desc = astroui.get_icon("CodeCompanion", 1, true) .. "CodeCompanion" }
+
+        maps.n[prefix .. "a"] = { "<Cmd>CodeCompanionActions<CR>", desc = "Action Palette" }
+        maps.v[prefix .. "a"] = { "<Cmd>CodeCompanionActions<CR>", desc = "Action Palette" }
+        maps.n[prefix .. "c"] = { "<Cmd>CodeCompanionChat Toggle<CR>", desc = "Toggle Chat" }
+        maps.v[prefix .. "c"] = { "<Cmd>CodeCompanionChat Toggle<CR>", desc = "Toggle Chat" }
+        maps.n[prefix .. "C"] = { "<Cmd>CodeCompanionChat<CR>", desc = "New Chat" }
+        maps.n[prefix .. "C"] = { "<Cmd>CodeCompanionChat<CR>", desc = "New Chat" }
+        maps.n[prefix .. "i"] = {
+          function()
+            vim.ui.input({ prompt = "Prompt: " }, function(input)
+              if input and input ~= "" then vim.cmd("CodeCompanion #buffer " .. input) end
+            end)
+          end,
+          desc = "Prompt Inline",
+        }
+        maps.v[prefix .. "i"] = { "<Cmd>'<,'>CodeCompanion<CR>", desc = "Prompt Inline" }
+        maps.v[prefix .. "e"] = { function() codecompanion.prompt "explain" end, desc = "Explain Code" }
+        maps.v[prefix .. "f"] = { function() codecompanion.prompt "fix" end, desc = "Fix Code" }
+        maps.n[prefix .. "l"] = { function() codecompanion.prompt "lsp" end, desc = "Explain LSP Diagnostics" }
+        maps.n[prefix .. "m"] = { function() codecompanion.prompt "commit" end, desc = "Generate Commit Message" }
+        maps.v[prefix .. "t"] = { function() codecompanion.prompt "tests" end, desc = "Generate Unit Tests" }
+
+        opts.autocmds.codecompanion = {
+          {
+            event = "User",
+            pattern = {
+              "CodeCompanionRequestStarted",
+              "CodeCompanionRequestFinished",
+            },
+            callback = function(args)
+              if args.match == "CodeCompanionRequestStarted" then
+                M.start_spinner()
+              elseif args.match == "CodeCompanionRequestFinished" then
+                M.stop_spinner()
+              end
+            end,
+          },
+        }
+      end,
+    },
+    { "AstroNvim/astroui", opts = { icons = { CodeCompanion = "󱚦" } } },
+  },
+  lazy = true,
+  cmd = { "CodeCompanion", "CodeCompanionActions", "CodeCompanionChat", "CodeCompanionCmd" },
+  opts = function(_, opts)
+    opts.adapters = {
+      gemini = function()
+        return require("codecompanion.adapters").extend("gemini", {
+          env = {
+            api_key = "cmd:pass api_key/gemini",
+          },
+        })
+      end,
+    }
+    opts.strategies = {
+      chat = {
+        adapter = "copilot",
+        keymaps = {
+          send = {
+            modes = { n = "<M-CR>", i = "<M-CR>" },
+          },
+          close = {
+            modes = { n = { "<C-q>", "q" }, i = {} },
+          },
+          stop = {
+            modes = { n = "<C-c>" },
+          },
+        },
+      },
+      inline = {
+        adapter = "copilot",
+      },
+    }
+    opts.display = {
+      diff = {
+        provider = "mini_diff",
+      },
+      chat = {
+        window = {
+          width = 0.3,
+        },
+        show_settings = true,
+      },
+    }
+    opts.strategies = {
+      chat = {
+        slash_commands = {
+          ["buffer"] = {
+            callback = "strategies.chat.slash_commands.buffer",
+            description = "Insert open buffers",
+            opts = {
+              contains_code = true,
+              provider = "fzf_lua", -- default|telescope|mini_pick|fzf_lua
+            },
+          },
+          ["file"] = {
+            callback = "strategies.chat.slash_commands.file",
+            description = "Insert a file",
+            opts = {
+              contains_code = true,
+              max_lines = 1000,
+              provider = "fzf_lua", -- default|telescope|mini_pick|fzf_lua
+            },
+          },
+          ["symbols"] = {
+            callback = "strategies.chat.slash_commands.symbols",
+            description = "Insert symbols for a selected file",
+            opts = {
+              contains_code = true,
+              provider = "fzf_lua", -- default|telescope|mini_pick|fzf_lua
+            },
+          },
+        },
+      },
+    }
+  end,
+  specs = {
+    {
+      "OXY2DEV/markview.nvim",
+      optional = true,
+      ft = function(_, ft) return require("astrocore").list_insert_unique(ft, { "codecompanion" }) end,
+    },
+  },
+}
--- dot_config/nvim/lua/plugins/markview.lua
+++ dot_config/nvim/lua/plugins/markview.lua
@@ -1,9 +1,10 @@
 return {
   "OXY2DEV/markview.nvim",
+  lazy = false,
   ft = function()
     local plugin = require("lazy.core.config").spec.plugins["markview.nvim"]
     local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-    return opts.filetypes or { "markdown", "quarto", "rmd", "latex" }
+    return opts.preview.filetypes or { "markdown", "quarto", "rmd", "latex" }
   end,
   dependencies = {
     { "nvim-tree/nvim-web-devicons" },
@@ -11,16 +12,45 @@ return {
       "nvim-treesitter/nvim-treesitter",
       opts = function(_, opts)
         if opts.ensure_installed ~= "all" then
-          opts.ensure_installed =
-              require("astrocore").list_insert_unique(opts.ensure_installed,
-                { "html", "markdown", "markdown_inline", "latex" })
+          opts.ensure_installed = require("astrocore").list_insert_unique(
+            opts.ensure_installed,
+            { "html", "markdown", "markdown_inline", "latex" }
+          )
         end
       end,
+    },
+  },
+  opts = function(_, opts)
+    local presets = require "markview.presets"
+    opts.preview = {
+      hybrid_modes = { "n" },
+      -- HACK: codecompanion must set in here
+      filetypes = { "markdown", "quarto", "rmd", "latex", "codecompanion" },
+      ignore_buftypes = {},
     }
-  },
-  opts = {
-    hybrid_modes = { "n" },
-    headings = { shift_width = 0 },
-    list_items = { shift_width = 0 },
-  },
+    opts.markdown = {
+      headings = presets.headings.marker,
+      tables = presets.tables.none,
+      list_items = {
+        shift_width = 0,
+        marker_minus = {
+          add_padding = false,
+          text = "•",
+        },
+        marker_plus = {
+          add_padding = false,
+          text = "▸",
+        },
+        marker_star = {
+          add_padding = false,
+          text = "◊",
+        },
+      },
+      code_blocks = {
+        style = "simple",
+        label_direction = "left",
+        sign = false,
+      },
+    }
+  end,
 }
