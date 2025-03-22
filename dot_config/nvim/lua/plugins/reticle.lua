347bf29b5c14ccc75e04bb42d67152f57ca827d6
Wings <wings.xiangyi.zeng@gmail.com>
2025-03-22 20:50:23 +0800

    nvim: add reticle to control cursor line and column

---
+++ dot_config/nvim/lua/plugins/reticle.lua
@@ -0,0 +1,52 @@
+-- BUG: Leave cmdline reset the cursor column
+-- auto open outline when resize window reset the cursor column
+-- why this only happens when toggle fullscreen?
+return {
+  "tummetott/reticle.nvim",
+  event = "User AstroFile",
+  dependencies = {
+    "AstroNvim/astrocore",
+    opts = {
+      autocmds = {
+        cursorcolumn_toggle_on_mode_change = {
+          {
+            event = { "ModeChanged" },
+            pattern = { "n:*" },
+            callback = function() vim.opt_local.cursorcolumn = false end,
+          },
+          {
+            event = { "ModeChanged" },
+            pattern = { "*:n" },
+            callback = function() vim.opt_local.cursorcolumn = true end,
+          },
+        },
+      },
+    },
+  },
+  opts = {
+    disable_in_insert = false,
+    disable_in_diff = false,
+    on_startup = {
+      cursorline = true,
+      cursorcolumn = true,
+    },
+    ignore = {
+      cursorline = {
+        "neo-tree",
+        "Outline",
+        "toggleterm",
+        "fzf",
+      },
+      cursorcolumn = {
+        "toggleterm",
+        "fzf",
+      },
+    },
+    never = {
+      cursorcolumn = {
+        "neo-tree",
+        "Outline",
+      },
+    },
+  },
+}
