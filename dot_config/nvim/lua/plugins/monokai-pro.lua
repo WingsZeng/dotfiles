8dff195516734687726211735063f9832adf7143
Wings <wings.xiangyi.zeng@gmail.com>
2025-03-24 23:18:53 +0800

    nvim: refactor monokai-pro color

--- dot_config/nvim/lua/plugins/monokai-pro.lua
+++ dot_config/nvim/lua/plugins/monokai-pro.lua
@@ -1,3 +1,19 @@
+local dark2 = "#111111"
+local dark1 = "#1a1a1a"
+local background = "#222222"
+local text = "#f8f8f2"
+local red = "#f92672"
+local orange = "#fd971f"
+local yellow = "#e6db74"
+local green = "#a6e22e"
+local blue = "#66d9ef"
+local purple = "#ae81ff"
+local dimmed1 = "#a2a2a2"
+local dimmed2 = "#888888"
+local dimmed3 = "#6f6f6f"
+local dimmed4 = "#555555"
+local dimmed5 = "#282828"
+
 return {
   "loctvl842/monokai-pro.nvim",
   dependencies = {
@@ -10,21 +26,21 @@ return {
     terminal_colors = false,
     overridePalette = function(_)
       return {
-        dark2 = "#111111",
-        dark1 = "#1a1a1a",
-        background = "#222222",
-        text = "#f8f8f2",
-        accent1 = "#f92672",
-        accent2 = "#fd971f",
-        accent3 = "#e6db74",
-        accent4 = "#a6e22e",
-        accent5 = "#66d9ef",
-        accent6 = "#ae81ff",
-        dimmed1 = "#a2a2a2",
-        dimmed2 = "#888888",
-        dimmed3 = "#6f6f6f",
-        dimmed4 = "#555555",
-        dimmed5 = "#282828",
+        dark2 = dark2,
+        dark1 = dark1,
+        background = background,
+        text = text,
+        accent1 = red,
+        accent2 = orange,
+        accent3 = yellow,
+        accent4 = green,
+        accent5 = blue,
+        accent6 = purple,
+        dimmed1 = dimmed1,
+        dimmed2 = dimmed2,
+        dimmed3 = dimmed3,
+        dimmed4 = dimmed4,
+        dimmed5 = dimmed5,
       }
     end,
   },
@@ -36,8 +52,7 @@ return {
         "AstroNvim/astroui",
         opts = function(_, opts)
           if not opts.highlights["monokai-pro"] then opts.highlights["monokai-pro"] = {} end
-          opts.highlights["monokai-pro"]["OutlineFoldMarker"] =
-            { fg = require("monokai-pro.colorscheme").get("classic").base.dimmed1 }
+          opts.highlights["monokai-pro"]["OutlineFoldMarker"] = { fg = dimmed1 }
         end,
       },
     },
@@ -48,10 +63,8 @@ return {
         "AstroNvim/astroui",
         opts = function(_, opts)
           if not opts.highlights["monokai-pro"] then opts.highlights["monokai-pro"] = {} end
-          opts.highlights["monokai-pro"]["MiniDiffSignAdd"] =
-            { fg = require("monokai-pro.colorscheme").get("classic").base.green }
-          opts.highlights["monokai-pro"]["MiniDiffSignDelete"] =
-            { fg = require("monokai-pro.colorscheme").get("classic").base.red }
+          opts.highlights["monokai-pro"]["MiniDiffSignAdd"] = { fg = green }
+          opts.highlights["monokai-pro"]["MiniDiffSignDelete"] = { fg = red }
           opts.highlights["monokai-pro"]["MiniDiffSignChange"] = { link = "Special" }
         end,
       },
