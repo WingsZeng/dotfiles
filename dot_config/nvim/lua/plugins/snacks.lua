292933b305523493cd17f93b8a82cc0e0cd39ff6
Wings <wings.xiangyi.zeng@gmail.com>
2025-08-13 14:31:35 +0800

    nvim: fix disable picker and disable gitbrowse

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -26,6 +26,7 @@ return {
         "                                      ░░░░░░          ",
       },
     },
-    picker = { enable = false },
+    picker = { enabled = false },
+    gitbrowse = { enabled = false },
   },
 }
